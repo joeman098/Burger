d08414d8f9e834227e73a8062158fe0d446246ee
joeman098 <Jlogue908@gmail.com>
2018-01-24 16:40:28 -0500

    almost

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,39 @@
 $(function() {
-
-$(".create-form").on("submit", function(event) {
+  $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newBurger = {
-      burger_name: $("#ca").val().trim(),
-      devoured: $("[name=sleepy]:checked").val().trim()
+      burger_name: $("#burgername")
+        .val()
+        .trim(),
+      devoured: $("[name=devoured]:checked")
+        .val()
+        .trim()
     };
-console.log(newBurger);
+    console.log(newBurger);
     // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
-    }).then(
-      function() {
-        console.log("New burger inputed!");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    }).then(function() {
+      console.log("New burger inputed!");
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 
+  $(".Eat").on("click", function(event) {
+    var id = $(this).data("id");
+    var devour = {
+      devoured: 1
+    };
+    console.log(id);
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devour
+    }).then(function() {
+      location.reload();
+    });
+  });
 });
